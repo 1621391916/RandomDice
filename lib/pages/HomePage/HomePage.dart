a828685f49eb13e2cc4a91509055c6ddea9a6ea6
WYY <1621391916@qq.com>
2022-11-23 13:56:09 +0800

    完成首页基本界面

--- lib/pages/HomePage/HomePage.dart
+++ lib/pages/HomePage/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './Buttons.dart';
+import 'package:random_dice_game/utils.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -100,18 +101,20 @@ class _RecommendSlidWidgetState extends State<RecommendSlidWidget> {
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 physics: const BouncingScrollPhysics(),
-                children: const [
+                children: [
                   RecommendUnit(
                     name: '《青楼》',
                     score: 8.8,
-                    coverUrl: 'https://s1.ax1x.com/2022/11/21/zlUGP1.png',
-                    tags: ['123', '456'],
+                    // coverUrl: 'https://s1.ax1x.com/2022/11/21/zlUGP1.png',
+                    cover: getImageFromPathOrUrl("assets/covers/cover1.png"),
+                    tags: const ['123', '456'],
                   ),
                   RecommendUnit(
                     name: '《青楼》',
                     score: 8.8,
-                    coverUrl: 'https://s1.ax1x.com/2022/11/21/zlUGP1.png',
-                    tags: ['123', '456'],
+                    // coverUrl: 'https://s1.ax1x.com/2022/11/21/zlUGP1.png',
+                    cover: getImageFromPathOrUrl("assets/covers/cover2.png"),
+                    tags: const ['123', '456'],
                   )
                 ],
               )
@@ -124,17 +127,19 @@ class _RecommendSlidWidgetState extends State<RecommendSlidWidget> {
 }
 
 class RecommendUnit extends StatelessWidget {
-  const RecommendUnit({
+  RecommendUnit({
     Key? key,
     required this.name,
-    required this.coverUrl,
+    // required this.coverUrl,
+    required this.cover,
     required this.score,
     required this.tags,
   }) : super(key: key);
 
-  final String name, coverUrl;
+  final String name;//, coverUrl;
   final double score;
   final List tags;
+  dynamic cover;
 
   @override
   Widget build(BuildContext context) {
@@ -157,7 +162,7 @@ class RecommendUnit extends StatelessWidget {
             Expanded(
               flex: 10,
               child: Image(
-                image: NetworkImage(coverUrl),
+                image: cover,//NetworkImage(coverUrl),
                 width: 100,
               )
             ),
@@ -217,7 +222,7 @@ class _RoomFilterWidgetState extends State<RoomFilterWidget> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 0.0, bottom: 20.0),
+      padding: const EdgeInsets.only(top: 0.0, bottom: 0.0),
       child: SizedBox(
         height: 70.0,
         child: Flex(
@@ -305,10 +310,115 @@ class RoomListWidget extends StatefulWidget {
 class _RoomListWidgetState extends State<RoomListWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 800,
-      color: Colors.red
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: 3,
+      // itemExtent: 200,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        return RoomUnit(
+          name: '红黑馆事件',
+          score: 8,
+          cover: getImageFromPathOrUrl('assets/covers/cover2.png'),
+          KP: '张三',
+          tags: const ['悬疑', '推理', '硬核', '现代'],
+          difficulty: 3,
+          roomCapacity: 5,
+          roomPeopleNum: 1
+        );
+      }
     );
   }
 }
 
+class RoomUnit extends StatelessWidget {
+  RoomUnit({
+    Key? key,
+    required this.name,
+    required this.score,
+    required this.cover,
+    required this.KP,
+    required this.tags,
+    required this.difficulty,
+    required this.roomCapacity,
+    required this.roomPeopleNum,
+  }) : super(key: key);
+
+  final String name, KP;
+  final int roomCapacity, roomPeopleNum;
+  final int difficulty;
+  final double score;
+  final List tags;
+  dynamic cover;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black26),
+        color: Colors.white,
+        borderRadius: BorderRadius.circular((10)),
+      ),
+      margin: const EdgeInsets.only(bottom: 20.0),
+      padding: const EdgeInsets.only(left: 0.0, bottom: 12),
+      height: 200.0,
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: [
+                const Spacer(flex: 1,),
+                Expanded(flex: 6, child: Text('《$name》')),
+                const Spacer(flex: 4,),
+                const Expanded(flex: 4, child: Text('评分：')),
+                Expanded(flex: 6, child: Text(score.toString()))
+              ],
+            )
+          ),
+          Expanded(
+            flex: 4,
+            child: Row(
+              children: [
+                const Spacer(flex: 1,),
+                Expanded(
+                  flex: 8,
+                  child: Image(
+                    image: cover,
+                    width: 150,
+                    height: 150,
+                    fit: BoxFit.fill,
+                  )
+                ),
+                const Spacer(flex: 1,),
+                Expanded(
+                  flex: 14,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Text('KP：$KP')
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Text('类型：${tags.join(' ')}')
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Text('难度：${['低', '中', '高'][difficulty-1]}')
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Text('房价人数：$roomPeopleNum/$roomCapacity')
+                      ),
+                    ],
+                  )
+                )
+              ],
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
